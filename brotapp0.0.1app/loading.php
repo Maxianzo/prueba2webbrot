2fe92f0382603edda5ff2c4881965b10a641b63a
maxianzo <104526106+Maxianzo@users.noreply.github.com>
2022-11-17 20:46:16 -0300

    Add files via upload

---
+++ brotapp0.0.1app/loading.php
@@ -0,0 +1,21 @@
+<?php
+//$_POST
+$usuario = $_POST ["user"];
+$contrasenia = $_POST ["pass"];
+
+$ckuser = "admin";
+$ckpass = "1234";
+
+//&& eso es Y   
+
+if ($usuario == $ckuser && $contrasenia == $ckpass) {
+    // print "entrasete pero ";
+    header("location:https://www.php.net/manual/es/language.operators.php");
+    }
+    
+    else {
+      //  print "error de contraseña";
+      header("location:erro.html");
+    }
+    
+?>
